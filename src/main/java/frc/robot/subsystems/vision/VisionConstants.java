22196b0d1741e275af414d2be3881b17963a9a94
Cjohn2312 <christopherhsjiang@gmail.com>
2026-01-24 11:14:48 -0800

    fixing naming error for limelight

--- src/main/java/frc/robot/subsystems/vision/VisionConstants.java
+++ src/main/java/frc/robot/subsystems/vision/VisionConstants.java
@@ -19,7 +19,7 @@ public class VisionConstants {
 
   // Camera names, must match names configured on coprocessor
   public static String camera0Name = "limelight-snap";
-  public static String camera1Name = "limeilight-crackle";
+  public static String camera1Name = "limelight-crackle";
   public static String camera2Name = "limelight-pop";
   public static String camera3Name = "limelight-jerk";
 
