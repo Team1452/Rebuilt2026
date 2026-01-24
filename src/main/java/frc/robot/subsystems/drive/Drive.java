848d50af351e3f98a3b5240c461ac4fbc3b058b5
nicky-mac <nmacnguyen26@windwardschool.org>
2026-01-24 11:25:56 -0800

    remove drive logging

--- src/main/java/frc/robot/subsystems/drive/Drive.java
+++ src/main/java/frc/robot/subsystems/drive/Drive.java
@@ -154,7 +154,6 @@ public class Drive extends SubsystemBase {
 
   @Override
   public void periodic() {
-    long startTime = System.nanoTime();
     odometryLock.lock(); // Prevents odometry updates while reading data
     gyroIO.updateInputs(gyroInputs);
     Logger.processInputs("Drive/Gyro", gyroInputs);
@@ -214,8 +213,6 @@ public class Drive extends SubsystemBase {
    // System.out.println("PITCH:" + gyroInputs.pitchPosition);
     //System.out.println("ROLL:" + gyroInputs.rollPosition);
      // Existing periodic code
-long endTime = System.nanoTime();
-Logger.recordOutput("Vision/PeriodicTime", (endTime - startTime) / 1e6); // In milliseconds logging for periodic time
   }
 
   /**
