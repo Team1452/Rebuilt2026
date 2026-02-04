4abc8febcd94d891b9b6dfe753ebc799911b9842
Cjohn2312 <christopherhsjiang@gmail.com>
2026-02-03 17:56:00 -0800

    removed previous candle systemexample

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,17 +5,15 @@ package frc.robot;
  */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "Rebuilt2026";
+  public static final String MAVEN_NAME = "Rebuilt2026-2";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 50;
-  public static final String GIT_SHA = "944803acc315cad0356dd056d9c805e84fd7b2a1";
-  public static final String GIT_DATE = "2026-02-03 19:14:28 EST";
-  public static final String GIT_BRANCH = "main";
-
-  public static final String BUILD_DATE = "2026-01-15 19:53:14 EST";
-  public static final long BUILD_UNIX_TIME = 1768524794131L;
+  public static final int GIT_REVISION = 53;
+  public static final String GIT_SHA = "d24aebc3c3fc376e7616f64d846e31a393067427";
+  public static final String GIT_DATE = "2026-02-03 20:50:49 EST";
+  public static final String GIT_BRANCH = "Main";
+  public static final String BUILD_DATE = "2026-02-03 20:55:32 EST";
+  public static final long BUILD_UNIX_TIME = 1770170132535L;
   public static final int DIRTY = 1;
-
 
   private BuildConstants(){}
 }
