51f2cdbab114eeae367546e5f64c0b6da33e74d3
Cjohn2312 <christopherhsjiang@gmail.com>
2026-02-10 17:18:55 -0800

    remove logging for test

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,14 +5,14 @@ package frc.robot;
  */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "Rebuilt2026";
+  public static final String MAVEN_NAME = "Rebuilt2026-2";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 59;
-  public static final String GIT_SHA = "84bf1c5ee99b893c521f2796f6610a3cd2c24467";
-  public static final String GIT_DATE = "2026-02-04 19:37:15 EST";
+  public static final int GIT_REVISION = 67;
+  public static final String GIT_SHA = "20b9c83fe20a9e0d683ee663f287b88d9eb3c756";
+  public static final String GIT_DATE = "2026-02-07 13:51:40 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-02-04 20:30:46 EST";
-  public static final long BUILD_UNIX_TIME = 1770255046243L;
+  public static final String BUILD_DATE = "2026-02-10 18:54:48 EST";
+  public static final long BUILD_UNIX_TIME = 1770767688632L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
