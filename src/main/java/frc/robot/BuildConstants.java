76daf02483d4bc06e2f52df90935daaac1a5d0ca
nicky-mac <nmacnguyen26@windwardschool.org>
2026-02-18 10:20:10 -0800

    hood + shooter

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,15 +5,15 @@ package frc.robot;
  */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "Rebuilt2026-2";
+  public static final String MAVEN_NAME = "Rebuilt2026-1";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 92;
-  public static final String GIT_SHA = "61b4070212646add26bd077f64149a15f140fab2";
-  public static final String GIT_DATE = "2026-02-17 13:34:53 EST";
+  public static final int GIT_REVISION = 93;
+  public static final String GIT_SHA = "9d25e1e7350a8a641baa0d567806d222c58ca3ed";
+  public static final String GIT_DATE = "2026-02-17 14:32:03 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-02-17 14:20:46 EST";
-  public static final long BUILD_UNIX_TIME = 1771356046646L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2026-02-18 13:16:46 EST";
+  public static final long BUILD_UNIX_TIME = 1771438606435L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
