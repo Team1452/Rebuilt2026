a4e726c3599fc673373f45d1a8b49d783ba80053
Cjohn2312 <christopherhsjiang@gmail.com>
2026-03-03 17:33:32 -0800

    remove useless autos and paths and added line up to bump climb, go over bump while spinning and normally

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt2026-2";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 116;
-  public static final String GIT_SHA = "8981f0b6551f977358e60af1e4268238e31a50bf";
-  public static final String GIT_DATE = "2026-03-02 21:52:48 EST";
+  public static final int GIT_REVISION = 118;
+  public static final String GIT_SHA = "c7793c7057a728e3a9b2dd3e13eef57f4faf2c71";
+  public static final String GIT_DATE = "2026-03-03 19:18:30 EST";
   public static final String GIT_BRANCH = "testbed";
-  public static final String BUILD_DATE = "2026-03-02 22:14:35 EST";
-  public static final long BUILD_UNIX_TIME = 1772507675300L;
+  public static final String BUILD_DATE = "2026-03-03 19:18:50 EST";
+  public static final long BUILD_UNIX_TIME = 1772583530622L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
