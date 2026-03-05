7856bb03dba2236cc7b9a17bde72464dc2ede0c0
Cjohn2312 <christopherhsjiang@gmail.com>
2026-03-04 18:23:29 -0800

    added robot stop that doesnt e-stop

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -21,6 +21,7 @@ import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
@@ -271,8 +272,9 @@ controller.leftBumper()
 
     controller.x().onTrue(Commands.parallel(Commands.runOnce(() -> ledSystem.setAnimation(AnimationType.Larson, 1)), (DriveCommands.getRunMyPathCommand("Back1"))));
 
-    
-
+    InstantCommand stopCommand = new InstantCommand(drive::stop, drive);
+      controller.a().onTrue(stopCommand);
+      
   }
 
   /**
