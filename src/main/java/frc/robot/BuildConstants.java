fbee4e7cc608bd090d7e42514d86a8a97e0240a2
Cjohn2312 <christopherhsjiang@gmail.com>
2026-03-06 18:08:41 -0800

    shooter paths for teleop

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Rebuilt2026-2";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 181;
-  public static final String GIT_SHA = "f0015d2a43e65a1d7396fe5a5d4442f591908302";
-  public static final String GIT_DATE = "2026-03-02 22:48:20 EST";
+  public static final int GIT_REVISION = 189;
+  public static final String GIT_SHA = "4e42b71058e90f9682549bb32e6086b743006547";
+  public static final String GIT_DATE = "2026-03-06 20:54:03 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-03-03 18:54:37 EST";
-  public static final long BUILD_UNIX_TIME = 1772582077918L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2026-03-06 20:55:11 EST";
+  public static final long BUILD_UNIX_TIME = 1772848511001L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
